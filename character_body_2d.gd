9476a587e8a8cfeaa94f2394fd7841ea0944ec7a
Fox <fox.hamren@gmail.com>
2025-05-10 14:28:54 +0200

    working

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -5,8 +5,8 @@ extends CharacterBody2D
 
 func get_input():
 	var input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	if(input_direction > 0):
-		velocity = input_direction * speed * acceleration
+	#if(input_direction > 0):
+	velocity = input_direction * speed# * acceleration
 
 func _physics_process(delta):
 	get_input()
