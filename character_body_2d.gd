e0481a557cfa82c101d2b6b4bc4eb7adc3a1188f
Fox <fox.hamren@gmail.com>
2025-05-10 15:17:46 +0200

    Speeen

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -4,6 +4,9 @@ extends CharacterBody2D
 
 @export var speed = 400
 @export var acceleration = 0
+@export var rotating = false
+@export var radians = 0
+@export var totalRot = 0
 
 func get_input(delta):
 	var input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
@@ -19,6 +22,17 @@ func get_input(delta):
 		if acceleration < 0:
 			acceleration = 0
 		velocity *= acceleration 
+	
+	if !rotating:
+		rotating = Input.is_action_pressed("space")
+		totalRot = 0
+	else:
+		radians = PI*delta
+		totalRot += radians
+		rotate(radians)
+		if totalRot > 2*PI:
+			rotating = false
+			rotation = 0
 
 func _physics_process(delta):
 	get_input(delta)
