faf8eb45d50436a71735654d0cadcdca6fb58adf
Fox <fox.hamren@gmail.com>
2025-05-10 14:57:09 +0200

    Fox-mergeconflict

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -3,11 +3,21 @@ extends CharacterBody2D
 @export var speed = 400
 @export var acceleration = 0
 
-func get_input():
+func get_input(delta):
 	var input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	#if(input_direction > 0):
-	velocity = input_direction * speed# * acceleration
+	if(abs(input_direction.x) > 0 || abs(input_direction.y) > 0):
+		if acceleration < 1:
+			acceleration += 0.5*delta
+		if acceleration > 1:
+			acceleration = 1
+		velocity = input_direction * speed * acceleration
+	else:
+		if acceleration > 0:
+			acceleration -= 0.7*delta
+		if acceleration < 0:
+			acceleration = 0
+		velocity *= acceleration 
 
 func _physics_process(delta):
-	get_input()
+	get_input(delta)
 	move_and_slide()
