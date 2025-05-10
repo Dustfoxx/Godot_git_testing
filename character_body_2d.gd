97e120d26779b332455c128f09da1820bb823179
atemmel <temmeladam@gmail.com>
2025-05-10 15:07:04 +0200

    commit

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -30,18 +30,18 @@ _|    _|  _|    _|  _|    _|    _|  _|  _|    _|  _|    _|
 
 func get_input(delta):
 	var input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	if(abs(input_direction.x) > 0 || abs(input_direction.y) > 0):
+	if(!(abs(input_direction.x) > 0 || abs(input_direction.y) > 0)):
+		if acceleration > 0:
+			acceleration -= 0.7*delta
+		if acceleration < 0:
+			acceleration = 0
+		velocity *= acceleration
+	else:
 		if acceleration < 1:
 			acceleration += 0.5*delta
 		if acceleration > 1:
 			acceleration = 1
 		velocity = input_direction * speed * acceleration
-	else:
-		if acceleration > 0:
-			acceleration -= 0.7*delta
-		if acceleration < 0:
-			acceleration = 0
-		velocity *= acceleration 
 
 func _physics_process(delta):
 	get_input(delta)
